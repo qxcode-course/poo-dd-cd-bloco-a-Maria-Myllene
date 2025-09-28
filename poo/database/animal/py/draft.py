de03a4b53e005ec3a59efef6a6c22f389146324b
Myllene Barbosa <myllenebar150@gmail.com>
2025-09-28 16:30:47 -0300

    Atividade do animal concluída com sucesso!

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -3,32 +3,42 @@ class Animal:
         self.species: str = species
         self.sound: str = sound
         self.age: int = 0
+
     def __str__ (self):
         return f"{self.species}:{self.age}:{self.sound}"
-    def grow (self):
-        if self.species >= 4:
-            self.species = 4
-            print(f"warning: {self.species} morreu")
-    def age (self, quantidade: int) -> None:
+    
+    def grow (self, quantidade: int) -> None:
         self.age += quantidade
+        if self.age >= 4:
+            print(f"warning: {self.species} morreu")
+            self.age = 4
+    def noise (self):
+        if self.age == 0:
+            print("---")
+        elif self.age >= 4:
+            print("RIP")
+        else:
+            print(self.sound)
 
 def main ():
     animal: Animal = Animal("", "")
     while True:
         line = input()
+        args: list[str] = line.split(" ")
         print("$" + line)
-        args = line.split(" ")
-
         if args[0] == "end":
             break
         elif args [0] == "init":
             species = args [1]
             sound = args [2]
             animal = Animal (species, sound)
-        elif args [0] == "grow":
-            animal.grow()
         elif args [0] == "show":
             print(animal)
+        elif args [0] == "grow":
+            amount: int = int(args[1])
+            animal.grow(amount)
+        elif args[0] == "noise":
+            animal.noise()
         else:
             print("fail: comando invalido")
 
