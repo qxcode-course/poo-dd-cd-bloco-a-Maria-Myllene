fa10dc5dd59c544ee9139e420b2f4adef484f7b7
Myllene Barbosa <myllenebar150@gmail.com>
2025-10-06 00:00:06 -0300

    atividade git prática feita

---
+++ poo/database/git_pratica/py/draft.py
@@ -0,0 +1,20 @@
+""" 
+Manipulando localmente (1 ponto) 1/1
+
+Primeiro fluxo de trabalho (1 ponto) 1/1
+
+Desfazendo mudanças (1 ponto) 1/1
+
+Olhando o histórico (1 ponto) 1/1
+
+Trabalhando remotamente (1 ponto) 1/1
+
+Criando um conflito (2 pontos) 1/2
+
+Usando máquina local (3 pontos) 3/3
+
+Fiz um repositório local e um remoto e pude manipulá-los, alterei e fiz vários commits e pushs, consegui aprender sobre 
+como voltar para um commit anterior e para que servia o git log que antes eu não possuía conhecimento. Entretanto, 
+tive dificuldades com o criar conflito pois não consegui realizá-lo devidamente, mas compreendo que, para tais casos, 
+usamos merge para resolver os conflitos gerais.
+"""
